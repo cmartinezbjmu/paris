6960d108ed5bf020f05547382547b67065dfbe08
cmartinezbjmu <cmartinezbedoya@gmail.com>
2019-08-18 21:47:44 -0500

    Se guardan datos en cache

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+
 (function () {
     'use strict';
 
@@ -11,6 +12,19 @@
         addDialog: document.querySelector('.dialog-container')
     };
 
+    let db = null;
+
+    const openReq = indexedDB.open('stationPreferences', 1);
+
+    openReq.onupgradeneeded = e => {
+        db = e.target.result;
+        db.createObjectStore('stations', { keyPath: 'key' });
+    }
+
+    openReq.onsuccess = e => {
+        db = e.target.result;
+        app.updateSchedules();
+    }
 
     /*****************************************************************************
      *
@@ -29,17 +43,39 @@
     });
 
     document.getElementById('butAddCity').addEventListener('click', function () {
-
-
         var select = document.getElementById('selectTimetableToAdd');
         var selected = select.options[select.selectedIndex];
         var key = selected.value;
         var label = selected.textContent;
-        if (!app.selectedTimetables) {
-            app.selectedTimetables = [];
-        }
-        app.getSchedule(key, label);
-        app.selectedTimetables.push({key: key, label: label});
+        app.getScheduleFromNetwork(key)
+            .then((response) => {
+                if (!response) {
+                    return;
+                }
+                var result = {};
+                result.key = key;
+                result.label = label;
+                result.created = response._metadata.date;
+                result.schedules = response.result.schedules;
+                app.updateTimetableCard(result);
+            });
+        var tx = db.transaction(["stations"], "readwrite");
+
+        // Informa sobre el éxito de la inicio de la transacción
+        tx.oncomplete = function (event) {
+            console.log('agregado con exito');
+        };
+
+        // Crea una almacén de objetos en la transacción
+        var objectStore = tx.objectStore("stations");
+
+        // Agrega nuestro objeto newItem al almacén de objetos
+        var objectStoreRequest = objectStore.add({ key: key, label: label });
+
+        objectStoreRequest.onsuccess = function (event) {
+            console.log('agregado con exito');
+        };
+
         app.toggleAddDialog(false);
     });
 
@@ -73,13 +109,7 @@
         var schedules = data.schedules;
         var card = app.visibleCards[key];
 
-        // CODELAB: Add code to call getForecastFromCache.
-        getForecastFromCache(schedules)
-        .then((forecast) => {
-        renderForecast(card, forecast);
-        });
-
-        if (!card) {
+        if (!card) { // creating
             var label = data.label.split(', ');
             var title = label[0];
             var subtitle = label[1];
@@ -90,14 +120,20 @@
             card.removeAttribute('hidden');
             app.container.appendChild(card);
             app.visibleCards[key] = card;
+        } else { // updating
+            var lastUpdated = card.querySelector('.card-last-updated').textContent
+            // If the data on the element is newer, skip the update.
+            if (lastUpdated >= dataLastUpdated) {
+                return;
+            }
         }
         card.querySelector('.card-last-updated').textContent = data.created;
 
         var scheduleUIs = card.querySelectorAll('.schedule');
-        for(var i = 0; i<4; i++) {
+        for (var i = 0; i < 4; i++) {
             var schedule = schedules[i];
             var scheduleUI = scheduleUIs[i];
-            if(schedule && scheduleUI) {
+            if (schedule && scheduleUI) {
                 scheduleUI.querySelector('.message').textContent = schedule.message;
             }
         }
@@ -115,107 +151,106 @@
      *
      ****************************************************************************/
 
-
-    app.getSchedule = function (key, label) {
-        var url = 'https://api-ratp.pierre-grimaud.fr/v3/schedules/' + key;
-
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function () {
-            if (request.readyState === XMLHttpRequest.DONE) {
-                if (request.status === 200) {
-                    var response = JSON.parse(request.response);
-                    var result = {};
-                    result.key = key;
-                    result.label = label;
-                    result.created = response._metadata.date;
-                    result.schedules = response.result.schedules;
-                    app.updateTimetableCard(result);
+    /**
+     * Get's the cached schedule data from the caches object.
+     *
+     * @param {string} key station reference.
+     * @return {Object} The schedule of the station, if the request fails, return null.
+     */
+    app.getScheduleFromCache = function (key) {
+        if (!('caches' in window)) {
+            return null;
+        }
+        const url = `/v4/schedules/${key}`;
+        return caches.match(url)
+            .then((response) => {
+                if (response) {
+                    return response.json();
                 }
-            } else {
-                // Return the initial weather forecast since no data is available.
-                app.updateTimetableCard(initialStationTimetable);
-            }
-        };
-        request.open('GET', url);
-        request.send();
+                return null;
+            })
+            .catch((err) => {
+                console.error('Error getting data from cache', err);
+                return null;
+            });
+    }
+
+
+    app.getScheduleFromNetwork = function (key, label) {
+        return fetch(`https://api-ratp.pierre-grimaud.fr/v4/schedules/${key}`)
+            .then((response) => {
+                return response.json();
+            })
+            .catch(() => {
+                return null;
+            });
     };
 
     // Iterate all of the cards and attempt to get the latest timetable data
     app.updateSchedules = function () {
-        var keys = Object.keys(app.visibleCards);
-        keys.forEach(function (key) {
-            app.getSchedule(key);
-        });
-        const data = JSON.stringify(keys);
-        localStorage.setItem('locationList', data);
-    };
-
-    /*
-     * Fake timetable data that is presented when the user first uses the app,
-     * or when the user has not saved any stations. See startup code for more
-     * discussion.
-     */
-
-    var initialStationTimetable = {
-        
-        key: 'metros/1/bastille/A',
-        label: 'Bastille, Direction La Défense',
-        created: '2017-07-18T17:08:42+02:00',
-        schedules: [
-            {
-                message: '0 mn'
-            },
-            {
-                message: '2 mn'
-            },
-            {
-                message: '5 mn'
+        var transaction = db.transaction(['stations'], "readonly");
+        var objectStore = transaction.objectStore('stations');
+        objectStore.openCursor().onsuccess = function (event) {
+            var cursor = event.target.result;
+            if (cursor) {
+                var item = cursor.value;
+                var key = item.key;
+                var label = item.label;
+                // Get the schedule data from the cache.
+                app.getScheduleFromCache(key)
+                    .then((response) => {
+                        if (!response) {
+                            return;
+                        }
+                        var result = {};
+                        result.key = key;
+                        result.label = label;
+                        result.created = response._metadata.date;
+                        result.schedules = response.result.schedules;
+                        app.updateTimetableCard(result);
+                    });
+                // Get the schedule data from the network.
+                app.getScheduleFromNetwork(key)
+                    .then((response) => {
+                        if (!response) {
+                            return;
+                        }
+                        var result = {};
+                        result.key = key;
+                        result.label = label;
+                        result.created = response._metadata.date;
+                        result.schedules = response.result.schedules;
+                        app.updateTimetableCard(result);
+                    });
+                cursor.continue();
+            } else {
+                console.log('Entries all displayed.');
             }
-        ]
-
-
+        };
     };
-
-
-    /************************************************************************
-     *
-     * Code required to start the app
-     *
-     * NOTE: To simplify this codelab, we've used localStorage.
-     *   localStorage is a synchronous API and has serious performance
-     *   implications. It should not be used in production applications!
-     *   Instead, check out IDB (https://www.npmjs.com/package/idb) or
-     *   SimpleDB (https://gist.github.com/inexorabletash/c8069c042b734519680c)
-     ************************************************************************/
-
-    app.getSchedule('metros/1/bastille/A', 'Bastille, Direction La Défense');
-    app.selectedTimetables = [
-        {key: initialStationTimetable.key, label: initialStationTimetable.label}
-    ];
-
-
-    /**
-     * Get's the cached forecast data from the caches object.
-     *
-     * @param {string} coords Location object to.
-     * @return {Object} The weather forecast, if the request fails, return null.
-     */
-    function getForecastFromCache(coords) {
-    // CODELAB: Add code to get weather forecast from the caches object.
-    if (!('caches' in window)) {
-      return null;
-    }
-    const url = `${window.location.origin}/forecast/${coords}`;
-    return caches.match(url)
-        .then((response) => {
-          if (response) {
-            return response.json();
-          }
-          return null;
-        })
-        .catch((err) => {
-          console.error('Error getting data from cache', err);
-          return null;
-        });
-    }
+    
+    app.getScheduleFromCache('metros/1/bastille/A')
+            .then((response) => {
+                if (!response) {
+                    return;
+                }
+                var result = {};
+                result.key = 'metros/1/bastille/A';
+                result.label = 'Bastille, Direction La Défense';
+                result.created = response._metadata.date;
+                result.schedules = response.result.schedules;
+                app.updateTimetableCard(result);
+            });
+        app.getScheduleFromNetwork('metros/1/bastille/A')
+            .then((response) => {
+                if (!response) {
+                    return;
+                }
+                var result = {};
+                result.key = 'metros/1/bastille/A';
+                result.label = 'Bastille, Direction La Défense';
+                result.created = response._metadata.date;
+                result.schedules = response.result.schedules;
+                app.updateTimetableCard(result);
+            });
 })();
